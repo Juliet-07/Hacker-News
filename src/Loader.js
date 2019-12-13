e9b141cb2e220d55d1e5c87f68c5760f7cb9838c
Juliet-07 <ohankwerejulietkelechi@gmail.com>
2019-12-13 09:37:07 +0100

    made changes in footer

---
+++ src/Loader.js
@@ -0,0 +1,5 @@
+import React, {Component} from 'react'
+import FadeIn from 'react-fade-in'
+import Lottie from 'react-lottie'
+import ReactLoading from 'react-loading'
+import 'bootstrap/dist/css/bootstrap.css'
